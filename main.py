a4a2b04688c6dab7be0d4ed3351cfee82d24132e
Jong Rock <whiteleo96@gmail.com>
2022-09-06 21:33:22 +0900

    fix Get bug | Add Post,Javascript

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI,Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse,RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pydantic import EmailStr,BaseModel
+from typing import Union
+
+class Data(BaseModel):
+    email: EmailStr
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static") 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_items(request: Request):
-    return templates.TemplateResponse('main.html',{"request" : request})
+async def main(request: Request,message:Union[None, str] = None):
+    return templates.TemplateResponse('main.html',{"request" : request,"message" :message})
+
+@app.post("/")
+async def main2(email: Data):
+    # email db check 후
+    # 중복된에 따른 다른 return
+    return RedirectResponse(url='/', status_code=302)
+
---
+++ static/js.js
@@ -0,0 +1,18 @@
+
+function sendJSON(){
+    var email = document.getElementById("email").value;
+    var regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    if (regEmail.test(email) == true){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        "email" : email
+    }));
+    alert(email + "가 등록되었습니다");
+    document.getElementById("email").value = '';}
+    else {
+        alert("올바른 이메일 주소를 입력해주세요")
+    }
+}
+
